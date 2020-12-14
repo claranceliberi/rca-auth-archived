4d81b7df32fef7305419916f9764d401ed7646b7
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-14 11:13:05 +0200

    add gitignore

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+
+app.get('/',(req,res) => {
+    res.send(' Express + Typescript servers')
+})
+    
+
+app.listen(PORT,()=>{
+    console.log(`🔥[Server]: server is runnng at https://localhost:${PORT}`)
+})
