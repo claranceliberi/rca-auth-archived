50cbfcd5dda60a79a82362661a6c4bd2cca41998
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-24 09:31:08 +0200

    new: initializing common.routes.config.ts

---
+++ src/common/common.routes.config.ts
@@ -0,0 +1,16 @@
+import express,{Application} from 'express'
+
+export class CommonRoutesConfig{
+    app:Application;
+    name:string;
+
+
+    constructor(app:Application,name:string) {
+        this.app = app
+        this.name = name
+    }
+
+    getName(){
+        return this.name
+    }
+}
