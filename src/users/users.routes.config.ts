6da9a753747cfd546f4d3c4be2c9151ab9f6e352
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-24 10:39:43 +0200

    new: creating routes

--- src/users/users.routes.config.ts
+++ src/users/users.routes.config.ts
@@ -1,5 +1,5 @@
 import {CommonRoutesConfig} from "../common/common.routes.config";
-import {Application} from 'express'
+import {Application,Request,Response,NextFunction} from 'express'
 
 
 export class UsersRoutes extends CommonRoutesConfig{
@@ -8,6 +8,37 @@ export class UsersRoutes extends CommonRoutesConfig{
     }
 
     configureRoutes(): Application {
+
+        //user route
+        this.app.route('/users')
+            .get((req: Request, res: Response) => {
+                res.status(200).send('list of all users')
+            })
+            .post((req: Request, res: Response) => {
+                res.status(200).send("a created user")
+            })
+
+
+        //user with id
+        this.app.route('/users/:userId')
+            .all((req: Request, res: Response, next: NextFunction) => {
+                // this middleware function runs before any request to /users/:userId
+                // but it doesn't accomplish anything just yet---
+                // it simply passes control to the next applicable function below using next()
+                next();
+            })
+            .get((req: Request, res: Response) => {
+                res.status(200).send(`GET requested for id ${req.params.userId}`);
+            })
+            .put((req: Request, res: Response) => {
+                res.status(200).send(`PUT requested for id ${req.params.userId}`);
+            })
+            .patch((req: Request, res: Response) => {
+                res.status(200).send(`PATCH requested for id ${req.params.userId}`);
+            })
+            .delete((req: Request, res: Response) => {
+                res.status(200).send(`DELETE requested for id ${req.params.userId}`);
+            });
 
         return this.app;
     }
