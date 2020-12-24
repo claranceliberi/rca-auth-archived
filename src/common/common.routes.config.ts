608b37195563ee52c6143e51e46e58c7bb8405af
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-24 10:26:11 +0200

    new: abstract and configureRoutes method

--- src/common/common.routes.config.ts
+++ src/common/common.routes.config.ts
@@ -1,16 +1,19 @@
 import {Application} from 'express'
 
-export class CommonRoutesConfig{
+export abstract class CommonRoutesConfig{
     app:Application;
     name:string;
 
 
-    constructor(app:Application,name:string) {
+    protected constructor(app:Application, name:string) {
         this.app = app
         this.name = name
+        this.configureRoutes()
     }
 
     getName(){
         return this.name
     }
+
+    abstract configureRoutes():Application
 }
