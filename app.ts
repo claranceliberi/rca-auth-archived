f246658225dffafdb5e20307802dff71a42b026d
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-26 19:53:35 +0200

    separate logger file

--- app.ts
+++ app.ts
@@ -2,19 +2,19 @@ import express,{Application,Response,Request} from 'express'
 import * as http from 'http'
 import * as bodyParser from "body-parser";
 
-import * as winston from "winston";
-import * as expressWinston from 'express-winston'
+
 import cors from 'cors'
+import {_errorLogger, console_logger, file_logger} from "./logger";
 import debug from "debug";
 
-import {CommonRoutesConfig} from "./src/common/common.routes.config";
-import  {UsersRoutes} from "./src/users/users.routes.config";
+import {CommonRoutesConfig} from "./src/routes/common/common.routes.config";
+import {UsersRoutes} from "./src/routes/users/users.routes.config";
 
 const app:Application = express();
 const server:http.Server = http.createServer(app)
-const port: Number = 3000;
+const port: Number = Number(process.env.PORT) || 5000;
 const routes: Array<CommonRoutesConfig> = []
-const debugLog: debug.IDebugger = debug('app')
+const debugLog: debug.IDebugger = debug('app');
 
 
 //parse all incoming requests as JSON
@@ -25,22 +25,17 @@ app.use(bodyParser.json())
 
 // here we are configuring the expressWinston logging middleware,
 // which will automatically log all HTTP requests handled by Express.js
-app.use(expressWinston.logger({
-    transports:[new winston.transports.Console()],
-    format: winston.format.combine(winston.format.colorize(), winston.format.json())
-}))
+app.use(file_logger)
+app.use(console_logger)
 
 
-// here we are adding the UserRoutes to our array,
+//adding the UserRoutes to our array,
 // after sending the Express.js application object to have the routes added to our app!
 routes.push(new UsersRoutes(app))
 
-// here we are configuring the expressWinston error-logging middleware,
+//configuring the expressWinston error-logging middleware,
 // which doesn't *handle* errors per se, but does *log* them
-app.use(expressWinston.errorLogger({
-    transports: [new winston.transports.Console()],
-    format: winston.format.combine(winston.format.colorize(), winston.format.json())
-}));
+app.use(_errorLogger);
 
 
 // this is a simple route to make sure everything is working properly
@@ -50,5 +45,9 @@ app.get('/', (req: Request, res: Response) => {
 
 
 server.listen(port,() =>{
-    console.log(`✨ Server has been started on https://localhost:${port}`)
+    debugLog(`✨ Server has been started on https://localhost:${port}`)
+
+    routes.forEach((route:CommonRoutesConfig) => {
+        debugLog(`Routes configured for "${route.getName()}"`)
+    })
 })
