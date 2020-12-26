274e0e7d175c32073f2663f5d05f2b90e1d81e90
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-26 19:47:17 +0200

    adding formats in logger

--- logger.ts
+++ logger.ts
@@ -2,19 +2,29 @@ import {transports,format} from "winston";
 import {logger,errorLogger} from 'express-winston'
 
 
+const { combine, timestamp, label, printf ,colorize, json, prettyPrint } = format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
 
 
-let _logger = logger({
+let file_logger = logger({
     transports:[
         new transports.File({
             maxsize:5120000,
             maxFiles:5,
             filename:`${__dirname}/logs/log-api.log`
         }),
-        new transports.Console()
     ],
     format: format.combine(format.colorize(), format.json())
 })
+
+let console_logger = logger({
+    transports:[new transports.Console()],
+    format: combine(colorize(),prettyPrint(), json(),timestamp(),myFormat)
+})
+
 
 let _errorLogger = errorLogger({
     transports: [new transports.Console()],
@@ -22,5 +32,6 @@ let _errorLogger = errorLogger({
 })
 
 export {
-    _logger, _errorLogger
+    file_logger, _errorLogger,
+    console_logger
 }
