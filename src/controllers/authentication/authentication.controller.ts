839d83327586734e803218e13f38da1575d64f02
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-27 23:53:21 +0200

    validating credentials

--- src/controllers/authentication/authentication.controller.ts
+++ src/controllers/authentication/authentication.controller.ts
@@ -1,7 +1,7 @@
 import {CommonControllerConfig} from "../common/common.controller.config";
 import {Request, Response} from "express";
 import {IUser, User} from "../../models/user.model";
-
+import bcrypt from 'bcryptjs'
 
 export class AuthenticationController extends CommonControllerConfig{
     constructor() {
@@ -15,9 +15,17 @@ export class AuthenticationController extends CommonControllerConfig{
             if(err)
                 res.send(s('failed',err,500))
             else if(user === null)
-                res.send(s('failed',"user not found",404))
-            else
-                res.send(s('success',user))
+                res.send(s('failed',"Wrong credentials",401))
+            else{
+                const truePassword:boolean = bcrypt.compareSync(req.body.password,user.password)
+
+                if(truePassword){
+                    res.send(s('success','thanks to login'))
+                } else {
+                    res.send(s('failed',"Wrong credentials",401))
+                }
+
+            }
         })
     }
 }
