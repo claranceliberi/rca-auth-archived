30259108e0d1f8dcd00727fb9c3543b4421b7171
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-27 23:48:46 +0200

    updating responses

--- src/controllers/authentication/authentication.controller.ts
+++ src/controllers/authentication/authentication.controller.ts
@@ -13,11 +13,11 @@ export class AuthenticationController extends CommonControllerConfig{
 
         let user = User.findOne({email:req.body.email},(err:any,user:IUser|null) => {
             if(err)
-                res.send(s('server error',err,500))
+                res.send(s('failed',err,500))
             else if(user === null)
-                res.send(s('no found',user,404))
+                res.send(s('failed',"user not found",404))
             else
-                res.send(s('found',user))
+                res.send(s('success',user))
         })
     }
 }
