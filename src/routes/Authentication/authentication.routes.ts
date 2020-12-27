2d0eef42b0565966bdb4fc0df15bcb5eb1d82390
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-27 23:32:05 +0200

    making auth routes with login method

---
+++ src/routes/Authentication/authentication.routes.ts
@@ -0,0 +1,18 @@
+import {CommonRoutesConfig} from "../common/common.routes.config";
+import {Application} from "express";
+import {AuthenticationController} from "../../controllers/authentication/authentication.controller";
+
+
+export class AuthenticationRoutes extends CommonRoutesConfig{
+    constructor(app:Application) {
+        super(app,"AuthenticationRoutes");
+    }
+
+    configureRoutes(): Application {
+        const auth = new AuthenticationController()
+
+        this.app.post('/auth/login',auth.login)
+
+        return this.app
+    }
+}
