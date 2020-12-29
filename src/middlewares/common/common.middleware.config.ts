e0207a51da1f9e79e34599185ab332e09295be27
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-29 10:38:36 +0200

    adding middleware name

--- src/middlewares/common/common.middleware.config.ts
+++ src/middlewares/common/common.middleware.config.ts
@@ -1,4 +1,23 @@
 
 export class CommonMiddlewareConfig{
+    name:string
 
+    constructor(name:string) {
+        this.name = name
+    }
+
+    getName(){
+      return  this.name
+    }
+
+    /**
+     * send formatted response
+     *
+     * @param msg
+     * @param data
+     * @param status
+     */
+    s(msg:string,data:any,status:number=200): object{
+        return {message:msg,data:data,status:status}
+    }
 }
