4f2b1e549f472fa96185fcd2967730aa39d5410a
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-29 23:59:35 +0200

    removing classes in constructors

--- src/routes/clients/clients.routes.ts
+++ src/routes/clients/clients.routes.ts
@@ -2,21 +2,21 @@ import {CommonRoutesConfig} from "../common/common.routes.config";
 import {Application} from "express";
 import {ClientsController} from "../../controllers/clients/clients.controller";
 
+const     cc:ClientsController = new ClientsController()
 
 export class ClientsRoutes extends CommonRoutesConfig{
-    cc:ClientsController        //clients controller
+
 
     constructor(app:Application) {
         super(app,'ClientsController');
 
-        this.cc = new ClientsController()
     }
 
-    configureRoutes = (): Application => {
+    configureRoutes(): Application {
 
         //creating app
         this.app.route('/clients')
-            .post(this.cc.create)
+            .post(cc.create)
 
         return this.app;
     }
--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -3,37 +3,41 @@ import {Application,Request,Response,NextFunction} from 'express'
 import {UsersController} from "../../controllers/user/users.controller";
 import {AuthenticateMiddleware} from "../../middlewares/authenticate/authenticate.middleware";
 
+//user controller
+const  uc: UsersController = new UsersController()
+
+//authenticated middleware
+const am: AuthenticateMiddleware  = new AuthenticateMiddleware()
+
 export class UsersRoutes extends CommonRoutesConfig{
-    uc: UsersController         //user controller
-    am: AuthenticateMiddleware  //authenticated middleware
+
 
     constructor(app:Application) {
         super(app,"UsersRoutes");
 
-        this.uc = new UsersController()
-        this.am = new AuthenticateMiddleware()
+
     }
 
     configureRoutes = (): Application => {
 
         //user route
         this.app.route('/users')
-            .get(this.am.authenticateToken,this.uc.all)
-            .post(this.uc.create)
-            .put(this.am.authenticateToken,this.uc.update)
+            .get(am.authenticateToken,uc.all)
+            .post(uc.create)
+            .put(am.authenticateToken,uc.update)
 
 
 
         //user with id
         this.app.route('/users/:userId')
-            .all(this.am.authenticateToken,(req: Request, res: Response, next: NextFunction) => {
+            .all(am.authenticateToken,(req: Request, res: Response, next: NextFunction) => {
                 // this middleware function runs before any request to /users/:userId
                 // but it doesn't accomplish anything just yet---
                 // it simply passes control to the next applicable function below using next()
                 next();
             })
-            .get(this.uc.get)
-            .delete(this.uc.delete);
+            .get(uc.get)
+            .delete(uc.delete);
 
 
             return this.app;
