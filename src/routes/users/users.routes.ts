a0bca1c49bf74600eecb636a9fafc9aff198b387
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-29 10:38:54 +0200

    adding authentication middleware

--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -1,9 +1,13 @@
 import {CommonRoutesConfig} from "../common/common.routes.config";
 import {Application,Request,Response,NextFunction} from 'express'
 import {UsersController} from "../../controllers/user/users.controller";
+import {AuthenticateMiddleware} from "../../middlewares/authenticate/authenticate.middleware";
 
 //initiating user controller
 const users = new UsersController()
+
+//initializing middleware
+const authMiddleware = new AuthenticateMiddleware()
 
 export class UsersRoutes extends CommonRoutesConfig{
     constructor(app:Application) {
@@ -14,15 +18,15 @@ export class UsersRoutes extends CommonRoutesConfig{
 
         //user route
         this.app.route('/users')
-            .get(users.all)
+            .get(authMiddleware.authenticateToken,users.all)
             .post(users.create)
-            .put(users.update)
+            .put(authMiddleware.authenticateToken,users.update)
 
 
 
         //user with id
         this.app.route('/users/:userId')
-            .all((req: Request, res: Response, next: NextFunction) => {
+            .all(authMiddleware.authenticateToken,(req: Request, res: Response, next: NextFunction) => {
                 // this middleware function runs before any request to /users/:userId
                 // but it doesn't accomplish anything just yet---
                 // it simply passes control to the next applicable function below using next()
