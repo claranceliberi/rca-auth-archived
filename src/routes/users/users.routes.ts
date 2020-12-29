4487090ac7164bc057e651eec737f7dabe8b2398
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-29 23:54:10 +0200

    declaring routes in constructor

--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -3,37 +3,37 @@ import {Application,Request,Response,NextFunction} from 'express'
 import {UsersController} from "../../controllers/user/users.controller";
 import {AuthenticateMiddleware} from "../../middlewares/authenticate/authenticate.middleware";
 
-//initiating user controller
-const users = new UsersController()
-
-//initializing middleware
-const authMiddleware = new AuthenticateMiddleware()
-
 export class UsersRoutes extends CommonRoutesConfig{
+    uc: UsersController         //user controller
+    am: AuthenticateMiddleware  //authenticated middleware
+
     constructor(app:Application) {
         super(app,"UsersRoutes");
+
+        this.uc = new UsersController()
+        this.am = new AuthenticateMiddleware()
     }
 
     configureRoutes(): Application {
 
         //user route
         this.app.route('/users')
-            .get(authMiddleware.authenticateToken,users.all)
-            .post(users.create)
-            .put(authMiddleware.authenticateToken,users.update)
+            .get(this.am.authenticateToken,this.uc.all)
+            .post(this.uc.create)
+            .put(this.am.authenticateToken,this.uc.update)
 
 
 
         //user with id
         this.app.route('/users/:userId')
-            .all(authMiddleware.authenticateToken,(req: Request, res: Response, next: NextFunction) => {
+            .all(this.am.authenticateToken,(req: Request, res: Response, next: NextFunction) => {
                 // this middleware function runs before any request to /users/:userId
                 // but it doesn't accomplish anything just yet---
                 // it simply passes control to the next applicable function below using next()
                 next();
             })
-            .get(users.get)
-            .delete(users.delete);
+            .get(this.uc.get)
+            .delete(this.uc.delete);
 
 
             return this.app;
