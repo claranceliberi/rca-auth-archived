dc1157756552f7e5e33af862ad8aafb4dc531976
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-29 11:44:31 +0200

    auth middleware for current user

--- src/routes/Authentication/authentication.routes.ts
+++ src/routes/Authentication/authentication.routes.ts
@@ -1,7 +1,9 @@
 import {CommonRoutesConfig} from "../common/common.routes.config";
 import {Application} from "express";
 import {AuthenticationController} from "../../controllers/authentication/authentication.controller";
+import {AuthenticateMiddleware} from "../../middlewares/authenticate/authenticate.middleware";
 
+const authMiddle = new AuthenticateMiddleware()
 
 export class AuthenticationRoutes extends CommonRoutesConfig{
     constructor(app:Application) {
@@ -12,7 +14,7 @@ export class AuthenticationRoutes extends CommonRoutesConfig{
         const auth = new AuthenticationController()
 
         this.app.post('/auth/login',auth.login)
-        this.app.get('/auth/current',auth.currentUser)
+        this.app.get('/auth/current',authMiddle.authenticateToken,auth.currentUser)
 
         return this.app
     }
