6def5f3350c962639a7dcc8232913ee9dc860f21
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-29 10:39:14 +0200

    making authentication middleware

--- src/middlewares/authenticate/authenticate.middleware.ts
+++ src/middlewares/authenticate/authenticate.middleware.ts
@@ -1,4 +1,26 @@
+import {CommonMiddlewareConfig} from "../common/common.middleware.config";
+import {NextFunction, Request, Response} from "express";
+import jwt from 'jsonwebtoken'
 
-export class AuthenticateMiddleware{
+export class AuthenticateMiddleware extends CommonMiddlewareConfig{
 
+    constructor() {
+        super('AuthenticationMiddleware');
+    }
+    authenticateToken = (req:Request,res:Response, next:NextFunction)=>{
+        //capturing authorization header to get token
+        const authHeader:string|undefined = req.header('Authorization')
+        const token:string|null|undefined = authHeader && authHeader.split(' ')[1]
+
+        //when does not exists return 401
+        if(!token) res.send(this.s('failed','token not found',401))
+        else {
+            jwt.verify(token,process.env["TOKEN_SECRETE"] as string,(err:any,user:any) => {
+                if(err)
+                    res.send(this.s('failed',err,500))
+                else
+                    next()
+            })
+        }
+    }
 }
