0187edb38bb9146fe378856e318c65e60b99f9af
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-29 23:54:43 +0200

    creating clients routes

---
+++ src/routes/clients/clients.routes.ts
@@ -0,0 +1,23 @@
+import {CommonRoutesConfig} from "../common/common.routes.config";
+import {Application} from "express";
+import {ClientsController} from "../../controllers/clients/clients.controller";
+
+
+export class ClientsRoutes extends CommonRoutesConfig{
+    cc:ClientsController        //clients controller
+
+    constructor(app:Application) {
+        super(app,'ClientsController');
+
+        this.cc = new ClientsController()
+    }
+
+    configureRoutes(): Application {
+
+        //creating app
+        this.app.route('/clients')
+            .post(this.cc.create)
+
+        return this.app;
+    }
+}
