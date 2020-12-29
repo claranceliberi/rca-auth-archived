d4a595f3fd1981e4bdadefb349102448630daa7e
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-29 21:42:28 +0200

    initiating class

---
+++ src/controllers/apps/apps.controller.ts
@@ -0,0 +1,11 @@
+import {CommonControllerConfig} from "../common/common.controller.config";
+import {Application} from "express";
+
+
+export class AppsController extends CommonControllerConfig{
+
+    constructor() {
+        super("AppsController");
+    }
+
+}
