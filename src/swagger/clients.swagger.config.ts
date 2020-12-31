c7eda543a77d80279dd4c141b9f4ecc94f4d02ad
ntwaricliberi <ntwaricliberi@gmail.com>
2020-12-31 23:04:24 +0200

    get all clients swagger

--- src/swagger/clients.swagger.config.ts
+++ src/swagger/clients.swagger.config.ts
@@ -3,5 +3,13 @@ import {Post, Route,Body,Security, Tags, Get, Path,Put, Delete} from "tsoa";
 @Route('clients')
 export class ClientsSwaggerConfig{
 
+    /**
+     * Get all clients
+     */
+    @Tags('clients')
+    @Get('/')
+    public async getAll(){
+
+    }
 
 }
