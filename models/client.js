ff2a7aaaca6a1335b8f2c0da668227e125c98bf0
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-03 23:28:58 +0200

    client model

---
+++ models/client.js
@@ -0,0 +1,46 @@
+'use strict';
+
+import {Model} from "sequelize";
+
+module.exports = (sequelize, DataTypes) => {
+  class Client extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+
+      Client.hasMany(models.App,{
+        foreignKey:'app',
+        as:"clientApps"
+      })
+    }
+  }
+
+  Client.init({
+    id:{
+      primaryKey:true,
+      autoIncrement:true,
+      type:DataTypes.BIGINT
+    },
+    firstName: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    secondName:{
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    email:{
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    password:{
+      type:DataTypes.STRING,
+      allowNull:false
+    }
+  }, {sequelize, modelName: 'Client'});
+
+  return Client;
+};
