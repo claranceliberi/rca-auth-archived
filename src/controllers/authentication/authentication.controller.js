8cef059b4e3b7a18af75ca6b45d5d42a51221d94
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-03 22:07:16 +0200

    removing typescript stuffs

--- src/controllers/authentication/authentication.controller.js
+++ src/controllers/authentication/authentication.controller.js
@@ -1,13 +1,12 @@
 import {CommonControllerConfig} from "../common/common.controller.config";
-import {Request, Response} from "express";
-import {IUser, User} from "../../models/user.model";
+import { User} from "../../models/user.model";
 import bcrypt from 'bcryptjs'
-import jwt,{Secret} from 'jsonwebtoken'
-import debug,{IDebugger} from 'debug'
+import jwt from 'jsonwebtoken'
+import debug from 'debug'
 import {Query} from "mongoose";
 import {AuthenticationSwaggerConfig} from "../../swagger/authentication.swagger.config";
 
-const d:IDebugger = debug('AuthController')
+const d = debug('AuthController')
 
 export class AuthenticationController extends CommonControllerConfig{
     constructor() {
@@ -17,19 +16,19 @@ export class AuthenticationController extends CommonControllerConfig{
     }
 
 
-    currentUser = async (req:Request,res:Response) => {
+    currentUser = async (req,res) => {
 
         //capturing authorization header to get token
-        const authHeader:string|undefined = req.header('Authorization')
-        const token:string|null|undefined = authHeader && authHeader.split(' ')[1]
+        const authHeader = req.header('Authorization')
+        const token = authHeader && authHeader.split(' ')[1]
 
         //get user email from token
-        const userFromToken:any = await jwt.verify(token as string,process.env["TOKEN_SECRETE"] as string)
+        const userFromToken = await jwt.verify(token ,process.env["TOKEN_SECRETE"])
 
         //excluding password in returned document
-        const user:Query<IUser, IUser, IUser>= User.where('email',userFromToken['email']).select('-password')
+        const user = User.where('email',userFromToken['email']).select('-password')
 
-        user.exec((err:any,user:IUser) => {
+        user.exec((err,user) => {
             if(err)
                 res.send(this.s('failed',err,500))
             else
@@ -40,22 +39,22 @@ export class AuthenticationController extends CommonControllerConfig{
 
     }
 
-    login = (req:Request,res:Response) =>{
+    login = (req,res) =>{
         const s = super.s
         const self = this
 
-        let user = User.findOne({email:req.body.email},(err:any,user:IUser|null) => {
+        let user = User.findOne({email:req.body.email},(err,user) => {
             if(err)
                 res.send(s('failed',err,500))
             else if(user === null)
                 res.send(s('failed',"Wrong credentials",401))
             else{
-                const truePassword:boolean = bcrypt.compareSync(req.body.password,user.password)
+                const truePassword = bcrypt.compareSync(req.body.password,user.password)
 
                 if(truePassword){
-                    const jwt:string = self.generatesAccessToken({email:req.body.email})
+                    const jwt = self.generatesAccessToken({email:req.body.email})
 
-                    const response:object = {
+                    const response = {
                         email:req.body.email,
                         token:jwt
                     }
@@ -69,8 +68,8 @@ export class AuthenticationController extends CommonControllerConfig{
         })
     }
 
-    generatesAccessToken = (username:string|object,secrete:string|undefined = process.env.TOKEN_SECRETE,expiresIn:string = '1800s'): string =>{
+    generatesAccessToken = (username,secrete = process.env.TOKEN_SECRETE,expiresIn = '1800s') =>{
         d(secrete)
-        return jwt.sign(username,<Secret>secrete,{expiresIn:expiresIn})
+        return jwt.sign(username,secrete,{expiresIn})
     }
 }
