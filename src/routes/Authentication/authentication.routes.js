f6cf5416e130895e9f45799c3aa5f59cd864c2fe
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-03 22:07:20 +0200

    removing typescript stuffs

--- src/routes/Authentication/authentication.routes.js
+++ src/routes/Authentication/authentication.routes.js
@@ -1,16 +1,15 @@
 import {CommonRoutesConfig} from "../common/common.routes.config";
-import {Application} from "express";
 import {AuthenticationController} from "../../controllers/authentication/authentication.controller";
 import {AuthenticateMiddleware} from "../../middlewares/authenticate/authenticate.middleware";
 
 const authMiddle = new AuthenticateMiddleware()
 
 export class AuthenticationRoutes extends CommonRoutesConfig{
-    constructor(app:Application) {
+    constructor(app) {
         super(app,"AuthenticationRoutes");
     }
 
-    configureRoutes(): Application {
+    configureRoutes(){
         const auth = new AuthenticationController()
 
         this.app.post('/auth/login',auth.login)
