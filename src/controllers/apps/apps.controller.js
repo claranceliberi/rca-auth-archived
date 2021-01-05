8b874bf079e14c70e4b9ea8d928f2c79a2f0dde9
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-05 14:21:03 +0200

    get all apps

--- src/controllers/apps/apps.controller.js
+++ src/controllers/apps/apps.controller.js
@@ -1,10 +1,25 @@
 const {CommonControllerConfig} = require("../common/common.controller.config")
+const models = require('../../database/postgresSql/models/index')
 
 
+const App = models.App
 class AppsController extends CommonControllerConfig{
 
     constructor() {
         super("AppsController");
+    }
+
+
+
+    //get all apps
+    all = async (req, res) => {
+            try{
+                const apps= await App.findAll() ;
+                res.send(this.s('success',apps))
+
+            }catch (e) {
+                res.send(this.s('failed',e,500))
+            }
     }
 
 }
