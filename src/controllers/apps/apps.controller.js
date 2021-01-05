95b21ba736e95105d34b1abbb4a76a1795a8e512
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-05 15:56:10 +0200

    creating app

--- src/controllers/apps/apps.controller.js
+++ src/controllers/apps/apps.controller.js
@@ -1,4 +1,5 @@
 const {CommonControllerConfig} = require("../common/common.controller.config")
+const {AuthenticationController} = require("../authentication/authentication.controller")
 const models = require('../../database/postgresSql/models/index')
 
 
@@ -21,6 +22,44 @@ class AppsController extends CommonControllerConfig{
                 res.send(this.s('failed',e,500))
             }
     }
+
+        //create client
+    create = async (req, res) => {
+        let {name,redirectUrl} = req.body
+
+
+        try{
+
+                //validator format
+                const schema = Joi.object({
+                    name:Joi.string().required().min(2),
+                    redirectUrl:Joi.string().uri().required().min(2),
+                })
+
+                const {error} = schema.validate(req.body)
+
+                //checking error
+                if(error)
+                    res.send(this.s('failed',error.details[0].message,409))
+
+                else{
+
+                    const randomNumber = Math.floor(Math.random() * Date.now())
+                    const appId = Date.now() + randomNumber //make sure that e get different number at the highest level
+                    let secretKey = crypto.randomBytes(64).toString('hex'); //secretKey
+                    let {id:clientId} = AuthenticationController.userFromToken(req)
+
+                    const result = await Client.create({name, redirectUrl, appId, secretKey,clientId})
+
+                    res.json(this.s('success',result))
+                }
+
+        }catch (e) {
+            res.json(this.s('failed',e,500))
+        }
+    }
+
+
 
 }
 
