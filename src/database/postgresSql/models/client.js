4d1e55b119e31bd75cecb0ac0535284a67393b54
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-05 22:53:42 +0200

    fixing relationship

--- src/database/postgresSql/models/client.js
+++ src/database/postgresSql/models/client.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
 
       Client.hasMany(models.App,{
-        foreignKey:'app',
+        foreignKey:'clientId',
         as:"clientApps"
       })
     }
