68880e5f7ee42d3d5d7431af6f2410896190f454
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-05 21:07:04 +0200

    configuring app routes

---
+++ src/routes/apps/app.routes.config.js
@@ -0,0 +1,46 @@
+const {CommonRoutesConfig} = require("../common/common.routes.config");
+const {AppsController} = require("../../controllers/apps/apps.controller");
+
+const app = new AppsController()
+
+class AppsRoutes extends CommonRoutesConfig{
+
+
+    constructor(app) {
+        super(app,'AppsRoutes');
+
+    }
+
+    configureRoutes() {
+
+        //creating apps
+        this.app.route('/apps')
+            .all((req,res,next) => {
+                next()
+            })
+            .post(app.create)
+            .put(app.put)
+            .get(app.all)
+
+
+        this.app.route('/apps/id/:id')
+            .all((req, res, next) => {
+                next()
+            })
+            .get(app.getById)
+
+
+
+        this.app.route('/apps/appId/:email')
+            .get(app.getByAppId)
+            .delete(app.delete)
+
+        this.app.route('/apps/client/:clientId')
+            .get(app.getByClient)
+
+
+        return this.app;
+    }
+}
+
+exports.ClientsRoutes = ClientsRoutes
