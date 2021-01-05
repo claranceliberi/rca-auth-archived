455eb5c7ccebe954a7410a10f2b8ddd3117f3eac
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-05 15:06:59 +0200

    changing model to Client

--- src/controllers/authentication/authentication.controller.js
+++ src/controllers/authentication/authentication.controller.js
@@ -1,12 +1,13 @@
 const {CommonControllerConfig} = require("../common/common.controller.config")
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const models = require('../../database/postgresSql/models/index')
 const debug = require('debug')
 
 const d = debug('AuthController')
 
-const User = require('../../models/user.model')
 
+const Client = models.Client
 class AuthenticationController extends CommonControllerConfig{
     constructor() {
         super("AuthenticationController");
@@ -24,7 +25,7 @@ class AuthenticationController extends CommonControllerConfig{
         const userFromToken = await jwt.verify(token ,process.env["TOKEN_SECRETE"])
 
         //excluding password in returned document
-        const user = User.where('email',userFromToken['email']).select('-password')
+        const user = Client.where('email',userFromToken['email']).select('-password')
 
         user.exec((err,user) => {
             if(err)
@@ -37,33 +38,35 @@ class AuthenticationController extends CommonControllerConfig{
 
     }
 
-    login = (req,res) =>{
+    login = async (req,res) =>{
         const s = super.s
         const self = this
 
-        let user = User.findOne({email:req.body.email},(err,user) => {
-            if(err)
-                res.send(s('failed',err,500))
-            else if(user === null)
-                res.send(s('failed',"Wrong credentials",401))
-            else{
-                const truePassword = bcrypt.compareSync(req.body.password,user.password)
+        let user = await Client.findOne({where:{email:req.body.email}})
 
-                if(truePassword){
-                    const jwt = self.generatesAccessToken({email:req.body.email})
+            try{
+                if(user === null)
+                    res.send(s('failed',"Wrong credentials",401))
+                else{
+                    const truePassword = bcrypt.compareSync(req.body.password,user.password)
 
-                    const response = {
-                        email:req.body.email,
-                        token:jwt
+                    if(truePassword){
+                        const jwt = self.generatesAccessToken({email:req.body.email})
+
+                        const response = {
+                            email:req.body.email,
+                            token:jwt
+                        }
+
+                        res.send(s('success',response))
+                    } else {
+                        res.send(s('failed',"Wrong credentials",401))
                     }
 
-                    res.send(s('success',response))
-                } else {
-                    res.send(s('failed',"Wrong credentials",401))
                 }
-
-            }
-        })
+        }catch(e) {
+            res.send(s('failed',e,500))
+        }
     }
 
     generatesAccessToken = (username,secrete = process.env.TOKEN_SECRETE,expiresIn = '1800s') =>{
