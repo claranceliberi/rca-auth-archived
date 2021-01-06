f67d99fd8e0ad54c8ff43f4d9c54884ee9bc9d9b
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-06 21:52:21 +0200

    create privilege

--- src/controllers/privillege/privilege.controller.js
+++ src/controllers/privillege/privilege.controller.js
@@ -24,7 +24,37 @@ class PrivilegeController extends CommonControllerConfig{
             }
     }
 
+    //create privilege
+    create = async (req, res) => {
+        let {userId,appID,viewProfile} = req.body
 
+
+        try{
+
+                //validator format
+                const schema = Joi.object({
+                    userId:Joi.string().required().min(20),
+                    appID:Joi.string().uri().required(),
+                    viewProfile:Joi.boolean().required(),
+                })
+
+
+                const {error} = schema.validate(req.body)
+
+                //checking error
+                if(error)
+                    res.send(this.s('failed',error.details[0].message,409))
+
+                else{
+
+                    const result = await Privilege.create({userId, appID , viewProfile})
+                    res.json(this.s('success',result))
+                }
+
+        }catch (e) {
+            res.json(this.s('failed',e,500))
+        }
+    }
 
 
 }
