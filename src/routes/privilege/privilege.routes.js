2be7b34dd4a964e48f0496eb849641a5003a754a
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-06 22:33:20 +0200

    setting privileges routes

---
+++ src/routes/privilege/privilege.routes.js
@@ -0,0 +1,45 @@
+const {CommonRoutesConfig} = require("../common/common.routes.config");
+const {PrivilegesController} = require("../../controllers/privillege/privilege.controller");
+
+const privilege = new PrivilegesController()
+
+class PrivilegeRoutes extends CommonRoutesConfig{
+
+
+    constructor(app) {
+        super(app,'PrivilegeRoutes');
+    }
+
+    configureRoutes() {
+
+        //creating apps
+        this.app.route('/privileges')
+            .all((req,res,next) => {
+                next()
+            })
+            .post(privilege.create)
+            .put(privilege.put)
+            .get(privilege.all)
+
+
+        this.app.route('/privileges/id/:id')
+            .all((req, res, next) => {
+                next()
+            })
+            .get(privilege.getById)
+
+
+
+        this.app.route('/privileges/appId/:appId')
+            .get(privilege.getByAppId)
+            .delete(privilege.delete)
+
+        this.app.route('/privileges/user/:userId')
+            .get(privilege.getByUserId)
+
+
+        return this.app;
+    }
+}
+
+exports.AppsRoutes = AppsRoutes
