4483522c49e69d0201b3ad3caf5327f8369b51e7
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-06 09:32:32 +0200

    creating privilege model and migration

---
+++ src/database/postgresSql/migrations/20210106071605-create-privilege.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Privileges', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      appId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'App',
+          key: 'id',
+          as: 'appId',
+        },
+      },
+      viewProfile: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Privileges');
+  }
+};
