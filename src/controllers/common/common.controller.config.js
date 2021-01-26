a34abf2fe7f3a762cd12a7493a4b5320e042a97f
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-26 18:49:20 +0200

    documenting class

--- src/controllers/common/common.controller.config.js
+++ src/controllers/common/common.controller.config.js
@@ -3,20 +3,31 @@ class CommonControllerConfig{
     name;
 
 
+    /**
+     * Parent class that has common controllers features
+     *
+     * @param name : name of the controller
+     */
     constructor( name) {
         this.name = name
     }
 
+    /**
+     * get name of the controller
+     * @returns {*}
+     */
     getName(){
       return  this.name
     }
 
     /**
-     * send formatted response
+     * Formatted output
      *
-     * @param msg
-     * @param data
-     * @param status
+     * @param msg : message to be outputted
+     * @param data : data (json) to be returned
+     * @param status : response status
+     *
+     * @returns {{data, message, status: number}}
      */
     s(msg,data,status=200){
         return {message:msg,data:data,status:status}
