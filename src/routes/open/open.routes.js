93fffb09a401c6c64c882d2c420caf52eb5857a4
ntwaricliberi <ntwaricliberi@gmail.com>
2021-01-26 23:26:43 +0200

    creating open routes

---
+++ src/routes/open/open.routes.js
@@ -0,0 +1,27 @@
+const {CommonRoutesConfig} = require("../common/common.routes.config");
+const {UserController} = require("../../controllers/user/users.controller");
+
+const user = new UsersController()
+
+class OpenRoutes extends CommonRoutesConfig{
+
+
+    constructor(app) {
+        super(app,'Openroutes');
+    }
+
+    configureRoutes() {
+
+
+        this.app.route('/v1/o/user/info')
+            .all((req,res,next) => {
+                next()
+            })
+            .post(user.get_user_by_token)
+
+
+        return this.app;
+    }
+}
+
+exports.PrivilegesRoutes = PrivilegesRoutes
