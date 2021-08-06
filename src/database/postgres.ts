6ff31b833967cc1178ebf78620a5a0d9596450fd
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-08-06 08:54:27 +0200

    refactor: delete unused code

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -7,15 +7,5 @@ export default new Sequelize(
     {
         host: process.env.POSTGRES_HOST,
         dialect: 'postgres',
-        // dialectOptions: {
-        //     connectTimeout: 60000,
-        // },
-        // pool: {
-        //     max: 100,
-        //     min: 0,
-        //     idle: 200000,
-        //     // @note https://github.com/sequelize/sequelize/issues/8133#issuecomment-359993057
-        //     acquire: 1000000,
-        // },
     },
 );
